921e0eb991b3afd6f8474bbfbd9fb3635e05ea59
Julia Wnuk <julia.m.wnuk@gmail.com>
2021-06-02 18:43:07 +0200

    Added corrcoef to female_votes_in_states

--- analysis/female_votes/us_election_female_votes_in_states.sql
+++ analysis/female_votes/us_election_female_votes_in_states.sql
@@ -216,16 +216,281 @@ ORDER BY
  * 
  * W partii Republikan w obu przypadkach wygrywa Donald Trump.*/
 
-/*UWAGI: Chcia³am podzieliæ na stany, gdzie kobiety stanowi¹ wiêkszoœæ i gdzie kobiety stanowi¹ mniejszoœæ,
- * jednak proporcje s¹ tutaj mocno zaburzone. Prawdopodobnie granicê postawiæ jako œrednia/mediana. 
- * 
- * Do przedyskutowania.*/
 
 
 
+/*procent kobiet zawiera siê w przedziale <47.4, 52.6>, dalsze rozwa¿ania przeprowadzone z podzia³em na poni¿ej/powy¿ej œredniej/mediany */
+/*liczba stanów, gdzie kobiety stanowi¹ wiêcej/mniej ni¿ œrednio w US */
+CREATE TEMP TABLE avg_med
+AS 
+SELECT
+  (SELECT cf2.sex255214 FROM county_facts cf2 WHERE cf2.fips = 0) AS us_avg,
+  percentile_disc(0.5) WITHIN GROUP(ORDER BY cf.sex255214) AS us_med 
+FROM
+  county_facts cf
+WHERE cf.state_abbreviation ISNULL ;
+
+SELECT *
+FROM
+  avg_med ;
+
+/*tabela z porównaniem populacji kobiet do wartoœci œredniej dla US oraz mediany*/
+CREATE OR REPLACE
+VIEW avg_med_comparison AS
+SELECT
+  cf.fips ,
+  cf.area_name ,
+  cf.sex255214 AS percent_women ,
+  (SELECT am.us_avg FROM avg_med am) ,
+  CASE
+    WHEN cf.sex255214 > am.us_avg THEN 'more than average'
+    WHEN cf.sex255214 < am.us_avg THEN 'less than average'
+    ELSE 'equals average'
+  END AS avg_check ,
+  (SELECT am.us_med FROM avg_med am) ,
+  CASE
+    WHEN cf.sex255214 > am.us_med THEN 'more than median'
+    WHEN cf.sex255214 < am.us_med THEN 'less than median'
+    ELSE 'equals median'
+  END AS med_check
+FROM
+  county_facts cf ,
+  avg_med am
+WHERE
+  cf.state_abbreviation ISNULL
+  AND cf.fips != 0
+ORDER BY
+  cf.fips ;
+
+SELECT * FROM avg_med_comparison ;
+
+/*ile stanów w poszczególnych przedzia³ach*/
+SELECT
+  med_check ,
+  count(*) AS states
+FROM
+  avg_med_comparison amc
+GROUP BY
+  med_check 
+ORDER BY 2 DESC ;
+
+--|med_check       |states|
+--|----------------|------|
+--|more than median|25    |
+--|less than median|24    |
+--|equals median   |2     |
 
 
+SELECT
+  avg_check ,
+  count(*) AS states
+FROM
+  avg_med_comparison amc
+GROUP BY
+  avg_check 
+ORDER BY 2 DESC ;
 
+--|avg_check        |states|
+--|-----------------|------|
+--|less than average|26    |
+--|more than average|23    |
+--|equals average   |2     |
+
+
+/*zestawienie rankingu kandydatów z populacj¹ kobiet*/
+SELECT
+  cpis.* ,
+  amc.percent_women ,
+  amc.us_avg ,
+  amc.avg_check ,
+  amc.us_med ,
+  amc.med_check
+FROM
+  candidates_percent_in_states cpis
+JOIN avg_med_comparison amc ON
+  cpis.state = amc.area_name
+ORDER BY
+  amc.fips ,
+  cpis.party ,
+  cpis.ranking ;
+
+/*ile razy poszczególni kandydaci zwyciê¿yli*/
+
+/*przedzia³ < us_avg*/
+SELECT
+  round(avg(amc.percent_women),2) AS avg_percent_women ,
+  cpis.party ,
+  cpis.candidate ,
+  count(cpis.state) AS states_won ,
+  round(avg(cpis.percent_votes),2) AS avg_votes_percent
+FROM
+  candidates_percent_in_states cpis
+JOIN avg_med_comparison amc ON
+  cpis.state = amc.area_name
+WHERE
+  cpis.ranking = 1
+  AND amc.avg_check LIKE 'less%'
+GROUP BY
+  cpis.party ,
+  cpis.candidate
+ORDER BY
+  cpis.party ,
+  4 DESC ;
+
+/*przedzia³ >= us_avg*/
+SELECT
+  round(avg(amc.percent_women),2) AS avg_percent_women ,
+  cpis.party ,
+  cpis.candidate ,
+  count(cpis.state) AS states_won ,
+  round(avg(cpis.percent_votes),2) AS avg_votes_percent
+FROM
+  candidates_percent_in_states cpis
+JOIN avg_med_comparison amc ON
+  cpis.state = amc.area_name
+WHERE
+  cpis.ranking = 1
+  AND (amc.avg_check LIKE 'more%' OR amc.avg_check LIKE 'equals%')
+GROUP BY
+  cpis.party ,
+  cpis.candidate
+ORDER BY
+  cpis.party ,
+  4 DESC ;
+
+
+/*przedzia³ < us_med*/
+SELECT
+  round(avg(amc.percent_women),2) AS avg_percent_women ,
+  cpis.party ,
+  cpis.candidate ,
+  count(cpis.state) AS states_won ,
+  round(avg(cpis.percent_votes),2) AS avg_votes_percent
+FROM
+  candidates_percent_in_states cpis
+JOIN avg_med_comparison amc ON
+  cpis.state = amc.area_name
+WHERE
+  cpis.ranking = 1
+  AND amc.med_check LIKE 'less%'
+GROUP BY
+  cpis.party ,
+  cpis.candidate
+ORDER BY
+  cpis.party ,
+  4 DESC ;
+
+/*przedzia³ >= us_med*/
+SELECT
+  round(avg(amc.percent_women),2) AS avg_percent_women ,
+  cpis.party ,
+  cpis.candidate ,
+  count(cpis.state) AS states_won ,
+  round(avg(cpis.percent_votes),2) AS avg_votes_percent
+FROM
+  candidates_percent_in_states cpis
+JOIN avg_med_comparison amc ON
+  cpis.state = amc.area_name
+WHERE
+  cpis.ranking = 1
+  AND (amc.med_check LIKE 'more%' OR amc.med_check LIKE 'equals%')
+GROUP BY
+  cpis.party ,
+  cpis.candidate
+ORDER BY
+  cpis.party ,
+  4 DESC ;
+
+--przedzia³ < us_avg
+--|avg_percent_women|party     |candidate      |states_won|avg_votes_percent|
+--|-----------------|----------|---------------|----------|-----------------|
+--|50.07            |Democrat  |Bernie Sanders |14        |61.98            |
+--|50.17            |Democrat  |Hillary Clinton|7         |55.26            |
+--|50.18            |Republican|Donald Trump   |13        |64.44            |
+--|50.18            |Republican|Ted Cruz       |6         |45.57            |
+
+--przedzia³ >= us_avg
+--|avg_percent_women|party     |candidate      |states_won|avg_votes_percent|
+--|-----------------|----------|---------------|----------|-----------------|
+--|51.21            |Democrat  |Hillary Clinton|18        |64.94            |
+--|50.90            |Democrat  |Bernie Sanders |2         |52.04            |
+--|51.18            |Republican|Donald Trump   |19        |46.26            |
+--|51.10            |Republican|John Kasich    |1         |47.57            |
+
+--przedzia³ < us_med
+--|avg_percent_women|party     |candidate      |states_won|avg_votes_percent|
+--|-----------------|----------|---------------|----------|-----------------|
+--|50.02            |Democrat  |Bernie Sanders |13        |62.71            |
+--|50.17            |Democrat  |Hillary Clinton|7         |55.26            |
+--|50.13            |Republican|Donald Trump   |12        |65.26            |
+--|50.18            |Republican|Ted Cruz       |6         |45.57            |
+
+--przedzia³ >= us_med
+--|avg_percent_women|party     |candidate      |states_won|avg_votes_percent|
+--|-----------------|----------|---------------|----------|-----------------|
+--|51.21            |Democrat  |Hillary Clinton|18        |64.94            |
+--|50.83            |Democrat  |Bernie Sanders |3         |52.19            |
+--|51.16            |Republican|Donald Trump   |20        |46.68            |
+--|51.10            |Republican|John Kasich    |1         |47.57            |
+
+/*Sprawdzenie korelacji dla poszczególnych kandydatów*/
+
+/*Ile razy kandydat znalaz³ siê na liœcie wyników*/
+SELECT
+  DISTINCT candidate ,
+  count(*)
+FROM
+  candidates_percent_in_states cpis
+JOIN county_facts cf ON
+  cpis.state = cf.area_name
+GROUP BY
+  candidate
+ORDER BY
+  2 DESC ;
+
+/*Zale¿noœæ wygranej kandydata od populacji kobiet badam dla kandydatów,
+ * którzy co najmniej 10 razy pojawiaj¹ siê w wynikach dla stanów*/
+
+CREATE OR REPLACE
+VIEW candidates_in_states_vs_female AS
+SELECT
+  cpis.* ,
+  cf.sex255214 AS percent_women ,
+  CASE
+    WHEN cpis.candidate = 'Bernie Sanders' THEN 1
+    WHEN cpis.candidate = 'Hillary Clinton' THEN 2
+    WHEN cpis.candidate = 'Donald Trump' THEN 3
+    WHEN cpis.candidate = 'John Kasich' THEN 4
+    WHEN cpis.candidate = 'Ted Cruz' THEN 5
+    WHEN cpis.candidate = 'Marco Rubio' THEN 6
+    WHEN cpis.candidate = 'Ben Carson' THEN 7
+    ELSE NULL
+  END AS candidate_number
+FROM
+  candidates_percent_in_states cpis
+JOIN county_facts cf ON
+  cpis.state = cf.area_name ;
+
+SELECT * FROM candidates_in_states_vs_female cisvf ;
+
+/*Wyniki*/
+SELECT
+  count(*) ,
+  candidate ,
+  corr(percent_votes, percent_women) ,
+  @corr(percent_votes, percent_women) AS corr_abs
+FROM
+  candidates_in_states_vs_female cisvf
+  WHERE candidate_number IS NOT NULL 
+GROUP BY candidate 
+ORDER BY 4 DESC ;
+
+/*Wniosek:
+ * - najwiêksz¹ zale¿noœæ widaæ w partii Demokratów. (0.71)
+ * - w partii Republikan dla wiêkszoœci kandydatów istnieje s³aba korelacja,
+ *   b¹dŸ praktycznie zupe³ny brak zwi¹zku miêdzy oddanymi na nich g³osami 
+ *   z populacj¹ kobiet w danym stanie, w przypadku Johna Kasich mo¿na stwierdziæ
+ *   nisk¹ korelacjê, w przypadku Bena Carsona - umiarkowan¹.*/
 
 
 
