51dcf69f5aa7e2e1081057c8df2d562a966dd7db
Kasia J <kjasielska@gmail.com>
2021-11-13 23:52:52 +0100

    x

--- 4-projekt-dl/app/api.py
+++ 4-projekt-dl/app/api.py
@@ -5,20 +5,32 @@
 import os
 from flask import Flask
 from flask import render_template, request
+import numpy as np
+import pathlib
+import PIL
+import os
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tensorflow import keras
+import warnings
+import sys 
+
+sys.path.append('../..')
 
 #IMPORT WSSYSTKICH IMPORTÓW POTRZEBNYCH DO MODELU I PREDYKCJI
-
 app = Flask(__name__)
 UPLOAD_FOLDER =  "/C:/Users/jk/Desktop/data science - infoshare/projekt_SQL/jdszr4-edc/4-projekt-dl/app/static"
 DEVICE = "cuda"
-MODEL = None
+MODEL = load_model('model_tl.h5')
 
 #PASTE MODEL HERE
 
 #PASTE PREDICT FUNCTION
 #def predict(image_path, model)
 
-@app.route("/template/", methods=["GET", "POST"])
+@app.route("/template", methods=["GET", "POST"])
 def upload_predict():
     if request.method == "POST":
         image_file = request.files["image"]
