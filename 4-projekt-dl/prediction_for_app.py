67e3b4cef2b0e17c083733c67a7f5e694481235a
Julia Wnuk <julia.m.wnuk@gmail.com>
2021-11-13 19:55:37 +0100

    Added parameter in class DLModel for image target size

--- 4-projekt-dl/prediction_for_app.py
+++ 4-projekt-dl/prediction_for_app.py
@@ -4,20 +4,29 @@ import PIL
 from keras.models import load_model
 
 class DLModel:
-    def __init__(self, model_path, weights_path):
+    def __init__(self, model_path, weights_path, dim: tuple):
         """
         Constructor of class DLModel.
         """
 
         self.model = load_model(model_path)
         self.model.load_weights(weights_path)
+        self.dim = dim
 
     def preprocess_image(self, image_path):
-        image = PIL.Image.open(str(image_path)).resize((224, 224))
+        """
+        Function takes path to file and prepares image for prediction in a way that model accepts it.
+        """
+
+        image = PIL.Image.open(str(image_path)).resize(self.dim)
         to_predict = np.array(image)
         return to_predict
 
     def predict_for_user_input(self, image_path):
+        """
+        Function predicts if the brain on the photo is a healthy one or with a tumor.
+        """
+
         to_predict = self.preprocess_image(image_path)
         prediction = self.model.predict(to_predict[np.newaxis, ...])
         return 'Healthy' if prediction > 0 else 'Brain tumor'
