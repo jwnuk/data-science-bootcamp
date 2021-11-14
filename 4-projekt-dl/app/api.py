40dec6189fb75c59ad9daba92aa1bf26e3bdd4a6
Kasia J <kjasielska@gmail.com>
2021-11-14 02:54:27 +0100

    another ver

--- 4-projekt-dl/app/api.py
+++ 4-projekt-dl/app/api.py
@@ -18,11 +18,10 @@ sys.path.append('../..')
 
 path_parent =os.path.dirname(os.getcwd())
 model_path = os.path.join(path_parent, 'C://Users//jk//Desktop//data science - infoshare//projekt_SQL//jdszr4-edc//4-projekt-dl//model_tl.h5')
-model_init  = pfa.DLModel(model_path, 'C://Users//jk//Desktop//data science - infoshare//projekt_SQL//jdszr4-edc//4-projekt-dl//weights//model_tl', (224, 224))
 
 app = Flask(__name__, template_folder='./template')
 UPLOAD_FOLDER =  ".../.../app/static"
-MODEL = models.load_model(model_init)
+MODEL = pfa.DLModel(model_path, 'C://Users//jk//Desktop//data science - infoshare//projekt_SQL//jdszr4-edc//4-projekt-dl//weights//model_tl', (224, 224))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -46,8 +45,8 @@ def predict():
                 image_file.filename
                 )
     image.save(image_location)
-    pred = model.predict(image_location, MODEL)[0]
-    return render_template("index.html", prediction =pred, image_loc = image_file.filename)
+    pred = MODEL.predict_for_user_input(image_location)[0]
+    return render_template("index.html", prediction = pred, image_loc = image_file.filename)
     
 if __name__ == "__main__":
     app.run(debug=True)
